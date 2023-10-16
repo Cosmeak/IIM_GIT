6e532ca7ce5f2698998a3d7de50a51d449174265
Cosmeak <guillaume.fine@cosmeak.dev>
2023-10-16 12:52:27 +0200

    [#2] feat: finalisation de l'inscription

--- validation.php
+++ validation.php
@@ -3,12 +3,27 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
-	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+if (!isset($_POST['username']) 
+	|| !isset($_POST['email']) 
+	|| !isset($_POST['password']) 
+	|| empty($_POST['username']) 
+	|| empty($_POST['email']) 
+	|| empty($_POST['password'])
+) {
+	$error = 'Erreur : Formulaire incomplet';
+}
 
-	// TODO
+if (!isEmailAvailable($db, $_POST['email'])) {
+	$error = 'Erreur : Email indisponible';
+}
 
-}else{ 
-	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
+if (!isUsernameAvailable($db, $_POST['username'])) {
+	$error = 'Erreur : Usernanme indisponible';
+}
+
+if (isset($error)) {
+	$_SESSION['message'] = $error;
 	header('Location: register.php');
 }
+
+userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']);
